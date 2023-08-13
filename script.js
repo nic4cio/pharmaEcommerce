d1899ef5310d4b8dcee874643cc6ecbae7172a08
nic4cio <gabrielnn_1@live.com>
2023-08-13 16:48:38 -0300

    dinamically add menu items

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 const logoElement = document.querySelector("#logo");
 logoElement.src = "./images/pharmaLogo.svg";
 
+const menuItems = [
+    { text: "Todos os remédios", link: "./index.html"},
+    { text: "Sobre nós", link: "#"},
+    { text: "Fale conosco", link: "./contato.html"}
+]
+
+const menuNavContainer = document.querySelector(".navbar-nav");
+menuItems.forEach(item => {
+    const li = document.createElement("li");
+    li.className = "nav-item";
+
+    const a = document.createElement("a");
+    a.className = "nav-link";
+    a.href = item.link;
+    a.textContent = item.text;
+
+    li.appendChild(a);
+    menuNavContainer.appendChild(li);
+});
+
+
+
 
