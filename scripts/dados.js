dad37f6696f8aa1c3808183e1978b55f171f057a
nic4cio <gabrielnn_1@live.com>
2023-08-13 20:21:13 -0300

    javascript file to store user data with an array of users without JSON format

---
+++ scripts/dados.js
@@ -0,0 +1,21 @@
+const usersData = `
+[
+    {
+        email: "teste1@teste.com",
+        name: "Usuário 1",
+        password: "1234567"
+    },
+    {
+        email: "teste2@teste.com",
+        name: "Usuário 2",
+        password: "1234567"
+    },
+    {
+        email: "teste3@teste.com",
+        name: "Usuário 3",
+        password: "1234567"
+    },
+]
+`;
+
+console.log(usersData)
